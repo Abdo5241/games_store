1aa5ca01f61aa4123759b01dad1e20e3df646cfa
Abdo5241 <nouhabdo55@gmail.com>
2024-01-13 21:04:20 +0200

    slider top

---
+++ animate.js
@@ -0,0 +1,2 @@
+import 'animate.css';
+document.documentElement.style.setProperty('--animate-duration', '1s');
---
+++ swipper_top.js
@@ -0,0 +1,10 @@
+// import Swiffy Slider JS
+import { swiffyslider } from 'swiffy-slider'
+window.swiffyslider = swiffyslider;
+
+window.addEventListener("load", () => {
+    window.swiffyslider.init();
+});
+
+// import Swiffy Slider CSS
+import "swiffy-slider/css"
